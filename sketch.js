5674f89a22675263175c4ab1542f0313a693208d
jessicow <jl14676@nyu.edu>
2023-12-11 16:06:56 -0500

    playing with physics
    trying to play with physics of sprites in order to create interactive relationships

--- sketch.js
+++ sketch.js
@@ -1,18 +1,20 @@
-let sprite; //using p5 play and p5 sound
+let sprite; //using p5 play and p5 sound - remember to push to repository regularly
+let test; 
 function setup() 
 {
 	createCanvas(800, 800);
 	background(255);
 
-	sprite = new Sprite(); 
-	sprite.width = 50;
-	sprite.height = 50; 
+	sprite = new Sprite(400, 400, 50, 50, 'kinematic'); //Sprite(x, y, w, h, collider type)
+	test = new Sprite(400, 400, 50, 50, 'static'); 
+
+	
 }
 
 function draw() {
 	clear(); 
 	
-	if(mouse.presses() && ((mouse.x>0 && mouse.x<800) && (mouse.y>0 && mouse.y <800))){
+	if(mouse.presses()){
 		sprite.moveTo(mouse.x, mouse.y, 9); 
 	}
 }
