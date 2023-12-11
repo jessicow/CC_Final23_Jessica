87c1bcabac4a6b7039567d3d7f2f67ded2556ea0
jessicow <jl14676@nyu.edu>
2023-12-11 15:40:05 -0500

    Attempting rpg
    creating the template for an rpg by first tackling the movement of the player sprite

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
-let sprite; 
-function setup(){
-    createCanvas(800, 800); 
-    background(255); 
+let sprite; //using p5 play and p5 sound
+function setup() 
+{
+	createCanvas(800, 800);
+	background(255);
 
-    sprite = new sprite(); 
-    sprite.width = 50;
-    sprite.height = 50; 
-
+	sprite = new Sprite(); 
+	sprite.width = 50;
+	sprite.height = 50; 
 }
 
-function draw(){
-
+function draw() {
+	clear(); 
+	
+	if(mouse.presses() && ((mouse.x>0 && mouse.x<800) && (mouse.y>0 && mouse.y <800))){
+		sprite.moveTo(mouse.x, mouse.y, 9); 
+	}
 }
