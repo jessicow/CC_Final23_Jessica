35d1bd237d3d097338953447b01a93adb130475b
jessicow <jl14676@nyu.edu>
2023-12-11 15:39:09 -0500

    Testing
    exploring the uses of sprites

--- sketch.js
+++ sketch.js
@@ -1,57 +1,14 @@
-let currentTime; 
-let lastTime;
-let interval; 
-
+let sprite; 
 function setup(){
-  createCanvas(800, 800); 
-  frameRate(40); 
-  // frameRate should be set once and should not be changed on the fly
-  // changing it on the fly is poor coding
-  lastTime = 0;
-  interval = 5000; 
+    createCanvas(800, 800); 
+    background(255); 
+
+    sprite = new sprite(); 
+    sprite.width = 50;
+    sprite.height = 50; 
+
 }
 
 function draw(){
-    /*if(frameCount <500){
-        //do nothing
-    }
-    
-    else if(frameCount<700){
-       scene1();
-    }
-    else if(frameCount <1000){
-       scene2(); 
-    }*/
-
-    if(currentTime-lastTime > interval)
-    {
-        lastTime = currentTime;
-        scene3(); 
-
-    }
-    print(frameCount); 
-
-}
-
-function scene1(){
-    background(0);
-}
-
-function scene2(){
-    background(0, 255, 0);
-
-}
-
-function scene3(){
-    background(0, 0, 255);
-
-}
-
-function scene4(){
-    background(127); 
-}
-
-function mousePressed(){
-    currentTime = millis(); //returns millis seconds
 
 }
