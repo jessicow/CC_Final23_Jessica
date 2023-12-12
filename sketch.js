ca50692735d1ea9d742dd4b8e1c54d20ee79763a
jessicow <jl14676@nyu.edu>
2023-12-12 15:02:54 -0500

    adding images + setting up basic functions

--- sketch.js
+++ sketch.js
@@ -1,42 +1,67 @@
 let iroh; //using p5 play and p5 sound - remember to push to repository regularly 
 let tree;
 let ground;
-let tombstone
+let tombstone;
+let minecraftFont;
+let continueForward; 
+
+let first, second, third, fourth, fifth; 
 
 function preload(){
-	tree = loadImg('green tree.png'); 
-	ground = loadImg('background_grass.png'); 
-	tombstone = loadImg('tombstone.png');
+	tree = loadImage('green tree.png'); 
+	ground = loadImage('background_grass.png'); 
+	tombstone = loadImage('tombstone.png');
+	pixelFont = loadFont('Minecraft.ttf'); 
 }
 
 function setup() 
 {
 	createCanvas(500, 500);
-	//background(ground);
 
 	iroh = new Sprite(250, 50, 20, 20, 'k'); //Sprite(x, y, w, h, collider type)
 	
+	first = true; 
+	
+	textFont(pixelFont); 
+	textSize(40); 
 
 }
 
 function draw() {
 	clear(); 
-	background(ground); 
+	
+	if(first){
+		background(ground); 
 	
 	tree.resize(150, 150); 
 	image(tree, 25, 50);
-	
-	tombstone.resize(100, 100); 
-	image(tombstone, 200, 230); 
-	
-	if(mouse.presses() && mouse.x<400){
-		iroh.moveTo(mouse.x, mouse.y, 5);  
+		
+		/*if((iroh.x>75 && iroh.x<150) && (iroh.y>60 && iroh.y<150)){
+			fill(0); 
+			text = new Sprite(250, 425, 400, 100, 's'); 
+			text.color = 'orange'; 
+			text.text = 'see memories?';
+		} */
+		
+		scene1(); 
 	}
 	
+	//tombstone.resize(100, 100); 
+	//image(tombstone, 200, 230); 
+	
+	if(mouse.presses()){
+		iroh.moveTo(mouse.x, mouse.y, 3);  
+	}
+	
+}
 
-	
-	
-	
-	
+function scene1(){
+	if((iroh.x>75 && iroh.x<150) && (iroh.y>60 && iroh.y<175)){
+			fill(0); 
+		  rect(50, 375, 400, 100);
+		
+		  fill(255);
+		  text('see memories?', 100, 425); 
+		}
 	
 }
