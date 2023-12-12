4e5da3929c330465a972f63c98b0354395a63db2
jessicow <jl14676@nyu.edu>
2023-12-11 19:13:45 -0500

    loading images/graphics
    playing with visuals

--- sketch.js
+++ sketch.js
@@ -1,20 +1,42 @@
-let sprite; //using p5 play and p5 sound - remember to push to repository regularly
-let test; 
+let iroh; //using p5 play and p5 sound - remember to push to repository regularly 
+let tree;
+let ground;
+let tombstone
+
+function preload(){
+	tree = loadImg('green tree.png'); 
+	ground = loadImg('background_grass.png'); 
+	tombstone = loadImg('tombstone.png');
+}
+
 function setup() 
 {
-	createCanvas(800, 800);
-	background(255);
+	createCanvas(500, 500);
+	//background(ground);
 
-	sprite = new Sprite(400, 400, 50, 50, 'kinematic'); //Sprite(x, y, w, h, collider type)
-	test = new Sprite(400, 400, 50, 50, 'static'); 
-
+	iroh = new Sprite(250, 50, 20, 20, 'k'); //Sprite(x, y, w, h, collider type)
 	
+
 }
 
 function draw() {
 	clear(); 
+	background(ground); 
 	
-	if(mouse.presses()){
-		sprite.moveTo(mouse.x, mouse.y, 9); 
+	tree.resize(150, 150); 
+	image(tree, 25, 50);
+	
+	tombstone.resize(100, 100); 
+	image(tombstone, 200, 230); 
+	
+	if(mouse.presses() && mouse.x<400){
+		iroh.moveTo(mouse.x, mouse.y, 5);  
 	}
+	
+
+	
+	
+	
+	
+	
 }
